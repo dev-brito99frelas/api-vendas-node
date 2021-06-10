c1fba59b822e786c49281f3c5dd76f808762f969
1cbzin-95 <clodoaldo.neto.116@ufrn.edu.br>
2021-06-10 12:19:44 -0300

    configurando inicio de projeto

---
+++ src/server.ts
@@ -0,0 +1,11 @@
+import express, { response } from 'express';
+
+const app = express();
+
+app.get('',(request,response)=>{
+    response.json({message:"Ola dev bem vindo de novo ao node typescript"});
+});
+
+app.listen(3333,()=>{
+    console.log("Api rodando ...");
+});
