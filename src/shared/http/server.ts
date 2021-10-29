9a5db76bd34a9a1fef07f4f693cdb95da4eebdbd
Nor-Program <clodoaldo.neto.116@ufrn.edu.br>
2021-10-29 14:03:30 -0300

    created conection by typeorm

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -3,6 +3,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '../errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 app.use(cors());
---
+++ src/shared/typeorm/index.ts
@@ -0,0 +1,5 @@
+import { createConnection } from 'typeorm';
+
+/**
+ * @description This method when started will be responsible for creating our project structure and find the ormconfig.json file and from it create our connection*/
+createConnection();
