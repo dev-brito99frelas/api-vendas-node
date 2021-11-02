527a21a0959a7c239fea8ee9502f35793bae7b2b
Brito-Response <clodoaldo.neto.116@ufrn.edu.br>
2021-11-02 14:04:07 -0300

    triggered the product routes

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,10 @@
 import { Router } from 'express';
+import productsRouter from '@modules/products/routes/Products.routes';
+
 
 const routes = Router();
+
+routes.use('/products', productsRouter);
 routes.get('/', (request, response) => {
   return response.json({ message: 'Sales api on' });
 });
